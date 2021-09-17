50c58c8e1d826b1aaa9857c4afcd5e447420cd6a
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-16 23:49:43 -0500

    update

--- index.js
+++ index.js
@@ -68,11 +68,3 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
 })
-// $(document).ready(function(){
-//     $('.content').click(function(){
-//       $('.content').toggleClass("heart-active")
-//       $('.text').toggleClass("heart-active")
-//       $('.numb').toggleClass("heart-active")
-//       $('.heart').toggleClass("heart-active")
-//     });
-//   });
